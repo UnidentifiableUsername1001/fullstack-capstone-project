3e0ac8776645981d50034a4085779df249aff9fb
UnidentifiableUsername1001 <klquinn12@gmail.com>
2026-03-25 09:38:29 -0400

    added routes for updating user info

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -7,9 +7,64 @@ const router = express.Router();
 const pino = require('pino');
 const dotenv = require('dotenv');
 const logger = pino();
-
-
 dotenv.config();
+
+// {Insert it along with other imports} Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+
+
+router.put('/update', async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+    const errors = validationResult(req);
+
+
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({errors: errors.array()});
+    }
+
+try {
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+    const email = req.headers.email;
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({ error: "Email not found in the request headers" });
+    }
+
+    // Task 4: Connect to MongoDB
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const existingUser = await collection.findOne({email});
+
+if (!existingUser) {
+    logger.error('No user found with this email');
+    return res.status(404).json({error: 'User not found"'});
+}
+
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+        {email},
+        {$set: existingUser},
+        {returnDocument: 'after'}
+    );
+
+    // Task 7: create JWT authentication using secret key from .env file
+    const payload = {
+        user: {
+            id: updatedUser._id.toString(),
+        },
+    };
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    res.json({authtoken});
+} catch (e) {
+    logger.error(e);
+     return res.status(500).send('Internal server error');
+
+}
+});
+
+
 
 //Step 1 - Task 4: Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
